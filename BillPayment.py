14a57f7e8a45137474515a1f015cc6367ea19f3c
Israel <israel@velocity.tech>
2023-10-28 18:04:59 +0200

    refactoring

--- BillPayment.py
+++ BillPayment.py
@@ -4,6 +4,6 @@ names = input('Enter names separated by a comma: ')
 
 names_list = names.split(',')
 
-random_int = random.randint(0, len(names_list))
+random_num = random.randint(0, len(names_list))
 
-print(f'{names_list[random_int]} is going to pay the bill')
+print(f'{names_list[random_num]} is going to pay the bill')
