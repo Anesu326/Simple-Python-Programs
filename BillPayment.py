6ed3d28b92b84d5f15304924906d9be99ed100d7
Israel <israel@velocity.tech>
2023-10-28 18:06:29 +0200

    update

--- BillPayment.py
+++ BillPayment.py
@@ -4,6 +4,6 @@ names = input('Enter names separated by a comma: ')
 
 names_list = names.split(',')
 
-random_num = random.randint(0, len(names_list))
+random_num = random.randint(0, len(names_list - 1))
 
 print(f'{names_list[random_num]} is going to pay the bill')
